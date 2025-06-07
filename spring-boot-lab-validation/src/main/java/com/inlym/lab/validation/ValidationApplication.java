e9f2552c57644dbafc213bad6d5901a0d5fb56a6
inlym <inlym@qq.com>
2025-06-07 21:15:16 +0800

    feat: add validation modules

---
+++ spring-boot-lab-validation/src/main/java/com/inlym/lab/validation/ValidationApplication.java
@@ -0,0 +1,18 @@
+package com.inlym.lab.validation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类
+ *
+ * @author inlym
+ */
+@SpringBootApplication
+public class ValidationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ValidationApplication.class, args);
+    }
+
+}
