b75f5f32b7997f9080d7d8bd6e0839295b91fed6
inlym <inlym@qq.com>
2025-06-11 21:35:08 +0800

    feat: add `docker` module

---
+++ docker/src/main/java/com/inlym/lab/docker/DockerApplication.java
@@ -0,0 +1,14 @@
+package com.inlym.lab.docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author inlym
+ */
+@SpringBootApplication
+public class DockerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DockerApplication.class, args);
+    }
+}
